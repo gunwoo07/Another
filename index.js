5379c1a98203aa9b9c2b49585f7be88df722928f
gunwoo07 <studentdeveloper07@gmail.com>
2022-07-23 18:34:59 +0900

    login basic setting

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const ejs = require("ejs");
+const loginRouter = require("./routers/Login");
 
 const port = 3000;
 
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use("/login", loginRouter);
 
 app.get("/", (req, res) => {
     res.send("Hello, welcome to Another!");
